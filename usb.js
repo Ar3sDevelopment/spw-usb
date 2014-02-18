22a425c07ed651dea12b7274d94c23cc0cf997bd
Matteo Bruni <ar3shack@gmail.com>
2014-02-18 13:48:31 +0100

    Prepared widget for new settings

--- usb.js
+++ usb.js
@@ -35,3 +35,7 @@ exports.data = function(cb) {
 exports.manage_post = function(post, cb) {
 	cb(0, null);
 };
+
+exports.updatetime = 1 * 60 * 60 * 1000;
+exports.title = 'USB Devices';
+exports.columns = 6;
